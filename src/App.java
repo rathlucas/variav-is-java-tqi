6319e94b625ed2ee4ba17d6b7863981f38c5c2e7
rathlucas <lucin189@gmail.com>
2022-06-15 21:29:47 -0300

    First commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Calculadora.sum(12, 16);
+
+        Calculadora.subtract(20, 10);
+
+        Calculadora.multiply(10, 10);
+
+        Calculadora.divide(8, 2);
+    }
+}
---
+++ src/Calculadora.java
@@ -0,0 +1,25 @@
+public class Calculadora {
+  public static void sum(double numberA, double numberB) {
+    double result = numberA + numberB;
+
+    System.out.println("A soma de " + numberA + " e " + numberB + " é igual a " + result);
+  }
+
+  public static void subtract(double numberA, double numberB) {
+    double result = numberA - numberB;
+
+    System.out.println("A subtração de " + numberA + " e " + numberB + " é igual a " + result);
+  }
+
+  public static void multiply(double numberA, double numberB) {
+    double result = numberA * numberB;
+
+    System.out.println("A multiplicação de " + numberA + " e " + numberB + " é igual a " + result);
+  }
+
+  public static void divide(double numberA, double numberB) {
+    double result = numberA / numberB;
+
+    System.out.println("A divisão de " + numberA + " e " + numberB + " é igual a " + result);
+  }
+}
